d6e937a6c805ca342a01e0f6a7df40a55f87c89d
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2024-06-22 11:01:19 -0600

    Project now shows up from drizzle

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import Card_Projects from '@/components/Card_Projects'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { projects } from '@/drizzle/schema';
+
+type Project = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  html_link: string;
+  github_link: string;
+};
 
 const ProjectsPage = () => {
+  const [projectList, setProjectList] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const sql = neon(process.env.NEXT_PUBLIC_POSTGRES_URL_NON_POOLING!);
+      const db = drizzle(sql);
+      const result = await db.select().from(projects);
+      setProjectList(result);
+    };
+    fetchProjects();
+  }, [])
+
   return (
     <div className='flex flex-col justify-center mb-[50px]'>
       <h1 className='mb-10 text-2xl text-center'>Projects</h1>
       <div className='flex gap-4 flex-wrap justify-center'>
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
-        <Card_Projects />
+        {projectList.map((project) => (
+          <Card_Projects key={project.id} project={project} />
+        ))}
       </div>
     </div>
   )
--- components/Card_Projects.tsx
+++ components/Card_Projects.tsx
@@ -2,29 +2,34 @@ import {
     Card,
     CardContent,
     CardFooter,
-    CardHeader,
 } from "@/components/ui/card"
 import Image from "next/image"
 
-const Card_Projects = () => {
+type Project = {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    html_link: string;
+    github_link: string;
+};
+
+const Card_Projects = ({ project }: { project: Project }) => {
   return (
     <Card className="w-[400px]">
-        <CardHeader>
-            <div className="relative">
-                <div className="flex space-x-2 absolute right-0">
-                    <a target="_blank" href={"/"}><Image src={"/images/chrome.png"} alt="Chrome Icon" width={28} height={28} /></a>
-                    <a target="_blank" href={"/"}><Image src={"/images/github.png"} alt="Github Icon" width={28} height={28} /></a>
-                </div>
-            </div>
-        </CardHeader>
         <CardContent>
-            <Image src={"/"} alt="Project Image" width={150} height={150} />
+            <div className="relative mt-8">
+                <div className="flex space-x-2 absolute right-0 p-2">
+                    <a target="_blank" href={project.html_link}><Image src={"/images/chrome.png"} alt="Chrome Icon" width={28} height={28} /></a>
+                    <a target="_blank" href={project.github_link}><Image src={"/images/github.png"} alt="Github Icon" width={28} height={28} /></a>
+                </div>
+                <Image src={project.image} alt="Project Image" width={400} height={400} priority/>
+            </div>
         </CardContent>
         <CardFooter>
             <div className="flex flex-col">
-                <p>Project Name</p>
-                <p>Project Description</p>
-                <p>Project Tags</p>
+                <p>{project.name}</p>
+                <p>{project.description}</p>
             </div>
         </CardFooter>
     </Card>
