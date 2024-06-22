86f999c7ac07fbb64601a3480932a1e532e7aaac
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2024-06-22 11:41:30 -0600

    changed css for better visual

--- components/Card_Projects.tsx
+++ components/Card_Projects.tsx
@@ -19,19 +19,17 @@ const Card_Projects = ({ project }: { project: Project }) => {
     <Card className="w-[400px]">
         <CardContent>
             <div className="relative mt-8">
-                <div className="flex space-x-2 absolute right-0 p-2">
-                    <a target="_blank" href={project.html_link}><Image src={"/images/chrome.png"} alt="Chrome Icon" width={28} height={28} /></a>
-                    <a target="_blank" href={project.github_link}><Image src={"/images/github.png"} alt="Github Icon" width={28} height={28} /></a>
+                <div className="flex space-x-1 absolute right-0 p-1 ">
+                    <a target="_blank" href={project.html_link} className="icon-container"><Image src={"/images/chrome.png"} alt="Chrome Icon" width={28} height={28} /></a>
+                    <a target="_blank" href={project.github_link} className="icon-container"><Image src={"/images/github.png"} alt="Github Icon" width={28} height={28} /></a>
                 </div>
                 <Image src={project.image} alt="Project Image" width={400} height={400} priority/>
+                <div className="flex flex-col mt-5">
+                    <h2 className="text-lg font-bold mb-2">{project.name}</h2>
+                    <p>{project.description}</p>
+                </div>
             </div>
         </CardContent>
-        <CardFooter>
-            <div className="flex flex-col">
-                <p>{project.name}</p>
-                <p>{project.description}</p>
-            </div>
-        </CardFooter>
     </Card>
   )
 }
